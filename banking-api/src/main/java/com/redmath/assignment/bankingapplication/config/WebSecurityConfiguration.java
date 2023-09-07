f38b10fb4f927e391df88caed605104ead59f8dc
Sami1150 <msamiahmed572@gmail.com>
2023-09-07 17:32:42 +0500

    Something is cooking

--- banking-api/src/main/java/com/redmath/assignment/bankingapplication/config/WebSecurityConfiguration.java
+++ banking-api/src/main/java/com/redmath/assignment/bankingapplication/config/WebSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.redmath.assignment.bankingapplication.config;
 
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,19 +8,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.*;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import static org.apache.catalina.core.ApplicationSessionCookieConfig.createSessionCookie;
+import java.util.concurrent.TimeUnit;
 
 @EnableMethodSecurity
 @CrossOrigin(origins = "http://localhost:3000",methods = {RequestMethod.GET,RequestMethod.POST, RequestMethod.DELETE,RequestMethod.PUT})
 @Configuration
 public class WebSecurityConfiguration {
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        return new HttpSessionCsrfTokenRepository();
+    }
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers(
@@ -29,6 +30,8 @@ public class WebSecurityConfiguration {
                 new AntPathRequestMatcher("/h2-console/**", "POST"),
                 new AntPathRequestMatcher("/actuator", "GET")
                 , new AntPathRequestMatcher("/actuator/**", "GET")
+//                , new AntPathRequestMatcher("/login/**", "GET")
+//                , new AntPathRequestMatcher("/logout/**", "POST")
         );
     }
 
@@ -36,13 +39,18 @@ public class WebSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.formLogin(formLogin -> Customizer.withDefaults());
+//        http.formLogin().loginPage("http://localhost:3000/login").permitAll();
         http.formLogin(formLogin->formLogin.defaultSuccessUrl("http://localhost:3000", true));
 //        http.formLogin(config -> config.successHandler(authenticationSuccessHandler()));
 //        http.logout(config->config.loginSuccessHandler((request, response, authentication) -> {}));
         http.authorizeHttpRequests(config -> config.anyRequest().authenticated());
         http.csrf(config -> config.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
+//        CsrfToken token = new HttpSessionCsrfTokenRepository().loadToken();
+        http.rememberMe()  //default to 2 weeks, if it is not present then it stays there for about 30 mins cuz after 30 mins session expires
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                .key("Something VerySuspiciousIsComing");
         return http.build();
-    }
 
+    }
 }
---
+++ banking-ui/src/Pages/Login.js
@@ -0,0 +1,28 @@
+// import React from 'react'
+
+// export default function Login() {
+//     return (
+//         <div>
+//             <title>Please sign in</title>
+//             <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossOrigin="anonymous" />
+//             <link href="https://getbootstrap.com/docs/4.0/examples/signin/signin.css" rel="stylesheet" crossOrigin="anonymous" />
+//             <div className="container">
+//                 <form className="form-signin" method="post" action="/login">
+//                 <h2 className="form-signin-heading">Welcome to Equity Bank</h2>
+
+//                     <h2 className="form-signin-heading">Please sign in</h2>
+//                     <p>
+//                         <label htmlFor="username" className="sr-only">Username</label>
+//                         <input type="text" id="username" name="username" className="form-control" placeholder="Username" required autofocus />
+//                     </p>
+//                     <p>
+//                         <label htmlFor="password" className="sr-only">Password</label>
+//                         <input type="password" id="password" name="password" className="form-control" placeholder="Password" required />
+//                     </p>
+//                     <input name="_csrf" type="hidden" defaultValue="a020c31b-03da-4352-b977-af63f9fb006f" />
+//                     <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
